315b99232698508b6bd95596c88c7396b9b49c95
pramenku <7664080+pramenku@users.noreply.github.com>
2024-07-23 09:00:54 +0530

    Update run.sh

--- examples/reduction/run.sh
+++ examples/reduction/run.sh
@@ -4,8 +4,8 @@ ldd ./reduction | grep -i asan
 K=1024*1024*4
 for i in {1..8}
 do
-    echo LD_PRELOAD=$(/opt/rocm/llvm/bin/clang -print-file-name=libclang_rt.asan-x86_64.so) ./$EXE $K
-    LD_PRELOAD=$(/opt/rocm/llvm/bin/clang -print-file-name=libclang_rt.asan-x86_64.so) ./$EXE $K
+    echo ./$EXE $K
+    ./$EXE $K
     K=$(($K*2))
 done
 
